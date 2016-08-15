b931f884d1d6012ac3268c2683a6dcd18db29546
Alex Beznos <beznosa@yahoo.com>
2016-08-15 09:59:47 +0300

    add search by uuis

--- app/controllers/v1/places_controller.rb
+++ app/controllers/v1/places_controller.rb
@@ -1,7 +1,11 @@
 module V1
   class PlacesController < ApplicationController
-    def index
-      @places = Place.all
+    def index # TODO: make this much better
+      @places = if params[:uuid]
+        Place.joins(:beacons).where("beacons.uuid in (?)", params[:uuid])
+      else
+        Place.all
+      end
 
       respond_with @places
     end
