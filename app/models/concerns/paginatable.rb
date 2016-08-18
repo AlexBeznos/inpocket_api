6d9b33afeea3572ec83db5c8a17cc625ed4c6fcd
Alex Beznos <beznosa@yahoo.com>
2016-08-18 15:28:08 +0300

    fix pagination

--- app/models/concerns/paginatable.rb
+++ app/models/concerns/paginatable.rb
@@ -2,7 +2,7 @@ module Paginatable
   extend ActiveSupport::Concern
 
   included do
-    scope :paginate, -> (params) do
+    scope :paginated, -> (params) do
       page(params[:page] || 1)
       .per(params[:per_page] || Kaminari.config.default_per_page )
     end
