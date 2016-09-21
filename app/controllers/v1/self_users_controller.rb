f98f6aef85d2076fa80de4e6bdd9caa9512288be
Alex Beznos <beznosa@yahoo.com>
2016-09-21 14:44:28 +0300

    add password

--- app/controllers/v1/self_users_controller.rb
+++ app/controllers/v1/self_users_controller.rb
@@ -19,7 +19,7 @@ module V1
     private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :photo)
+      params.require(:user).permit(:first_name, :last_name, :photo, :password, :email)
     end
   end
 end
