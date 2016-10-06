be90f483eaa349cbfca97ed47863ee4680c7bed7
Alex Beznos <beznosa@yahoo.com>
2016-10-06 11:38:57 +0300

    change action_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   include Authenticatable
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
--- app/controllers/v1/debut_presents_controller.rb
+++ app/controllers/v1/debut_presents_controller.rb
@@ -1,5 +1,6 @@
 module V1
   class DebutPresentsController < ApplicationController
+
     def show
       @present = Present.filter(params.slice(:place_id))
                         .find_by(debut: true)
